764912f50a898172ba84c125d4fc614f2ddafa0e
Melike <melikebolen1@gmail.com>
2022-06-29 11:51:22 +0300

    Initial Commit

---
+++ MovieSearchOmdb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieSearchOmdb
+//
+//  Created by Melike Bolen on 6/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
